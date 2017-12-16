c24e878268a38e8e365fc146b15adc483311ee44
purple-jwl <purple.jwl@gmail.com>
2017-12-16 11:55:48 +0900

    add GTID replication config

--- slave/setup.sh
+++ slave/setup.sh
@@ -4,9 +4,10 @@ until mysqladmin ping -h master --silent; do
     sleep 1
 done
 
+# Not required for GTID replication
 log_file=`mysql -u root -h master -e 'SHOW MASTER STATUS \G' | grep File: | awk '{print $2}'`
 pos=`mysql -u root -h master -e 'SHOW MASTER STATUS \G' | grep Position: | awk '{print $2}'`
 
 mysql -u root -e "CHANGE MASTER TO MASTER_HOST='master', MASTER_USER='repl', MASTER_PASSWORD='repl', MASTER_LOG_FILE='${log_file}', MASTER_LOG_POS=${pos}"
-
+# mysql -u root -e "CHANGE MASTER TO MASTER_HOST='master', MASTER_USER='repl', MASTER_PASSWORD='repl', MASTER_AUTO_POSITION=1"
 mysql -u root -e 'START SLAVE'
